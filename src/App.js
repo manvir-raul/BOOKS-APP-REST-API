68190dc7e68691ae0cf7e1563511a6eb36aa4cda
manvirraul <manvirsinghraul@gmail.com>
2019-08-28 09:22:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      modal: false,
+      // book:{id:null,title:"",rating:null}
+      title: "",
+      rating: null,
+      id:1
+    };
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  };
+
+  // onChange=(e)=>{
+
+  //   // this.setState({
+  //   //   book:{[e.target.name]:e.target.value}
+  //   // })
+  //   console.log("e,target.value=",e.target.name);
+  //   // this.setState((prevState)=>{
+  //   //   console.log("e,target=",e.target)
+  //   //   return ({
+  //   //               book:{...prevState.book,
+  //   //                    [e.target.name]:e.target.value
+  //   //               }
+  //   //           })
+  //   //         }
+  //   // )
+  // }
+
+  onChangeTitle = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onDelete=(id)=>{
+    console.log("onDelete =id",id)
+    this.setState((prevState)=>{
+      let books=prevState.books;
+      books.splice(id,1)
+      return ({books:books})
+    })
+  }
+
+  onEdit=(id)=>{
+
+  }
+  onSubmit = () => {
+    this.toggle();
+    this.setState(prevState => {
+      let book = {
+        title: prevState.title,
+        rating: prevState.rating,
+        id: prevState.id
+      };
+      let books=[...prevState.books];
+      books.push(book)
+      console.log("book", book);
+
+      return ({ books: books,title:"",rating:null,id:++prevState.id});
+    });
+  };
+
+  render() {
+    const books = this.state.books.map((book,index) => {
+      return (
+        <tr key={book.id}>
+          <td>{index+1}</td>
+          <td>{book.title}</td>
+          <td>{book.rating}</td>
+          <td>
+            <Button color="success" size="sm" className="mr-2">
+              Edit
+            </Button>
+            <Button color="danger" size="sm" onClick={()=>this.onDelete(index)}>
+              Delete
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+
+    return (
+      <div className="App">
+        <div>
+          <Button color="primary" onClick={this.toggle}>
+            Add book +
+          </Button>
+        </div>
+        <div>
+          <Table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>title</th>
+                <th>rating</th>
+                <th>action</th>
+              </tr>
+            </thead>
+            <tbody>{books}</tbody>
+          </Table>
+        </div>
+        <Modal
+          isOpen={this.state.modal}
+          //toggle={this.toggle}
+          className={this.props.modal}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <ModalHeader toggle={this.toggle}>Add Book</ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="exampleText">Book</Label>
+              <Input
+                type="text"
+                name="title"
+                id="exampleText"
+                placeholder="with a book"
+                onChange={this.onChangeTitle}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleRating">Rating</Label>
+              <Input
+                type="number"
+                name="rating"
+                id="exampleNumber"
+                placeholder="Rating placeholder"
+                onChange={this.onChangeTitle}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.onSubmit}>
+              Add Book
+            </Button>{" "}
+            <Button color="secondary" onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
